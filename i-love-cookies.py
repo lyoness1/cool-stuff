f43be5f82497ec8d6c87158ba09345f07d549a3a
Hackbright Student <info@hackbrightacademy.com>
2016-04-07 13:01:05 -0700

    Reverse string progress

---
+++ i-love-cookies.py
@@ -0,0 +1,15 @@
+"I love  cookies"
+"cookies  love I"
+
+def reverse_words():
+    '''reverses the words, including extra spaces'''
+    original_sentence = raw_input("What sentence would you like to reverse? ")
+    char_list = []
+    for x in original_sentence:
+        char_list.append(x)
+    print char_list
+    for item in char_list:
+        if item ==
+
+
+reverse_words()
